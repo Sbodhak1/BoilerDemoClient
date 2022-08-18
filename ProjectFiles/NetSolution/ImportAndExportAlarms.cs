d8567c1a47cb7d1e7c299b4e34839f698fb1ccf4
Sbodhak1 <subhajit.bodhak@rockwellautomation.com>
2022-08-18 16:20:07 +0530

    Deploying

---
+++ ProjectFiles/NetSolution/ImportAndExportAlarms.cs
@@ -0,0 +1,27 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.OPCUAClient;
+using FTOptix.WebUI;
+using FTOptix.OPCUAServer;
+using FTOptix.NetLogic;
+using FTOptix.CoreBase;
+using FTOptix.EventLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.Alarm;
+using FTOptix.Core;
+#endregion
+
+public class ImportAndExportAlarms : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
